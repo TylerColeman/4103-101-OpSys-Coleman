9310fb93e3f82287ce6733d561ad670f2402bd1d
Tyler Coleman <tylerblake.tc@gmail.com>
2016-09-12 20:10:57 -0500

    Sum_args fin

--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -1,1 +1,17 @@
-dsada
+#!/bin/bash
+sum=0
+x=0
+z=$#
+for y in $@
+do
+	let sum+=$y
+	echo -n $y
+	let x+=1
+	if [ $x -lt $z ]
+	then
+		echo -n " "+" "
+	else
+		echo -n " "=" " 
+	fi
+done
+echo $sum
